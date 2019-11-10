cb463e90f689c6159d6810f7aa2564d273a55d23
Darya Podoprikhina <darya290499@yandex.ru>
2019-11-10 23:26:12 +0300

    2 задачи

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 /*Ввести два одномерных целочисленных массива A и B из N и M элементов
 соответственно. Сформировать массив С, добавив в него сначала элементы
 массива А, затем элементы массива B, кратные числу К.
-
+ *
     Ввести одномерный целочисленный массив A. Сформировать массив B, записав в
 него все элементы массива А, стоящие после минимального. Массив B
 отсортировать по убыванию.*/
@@ -15,18 +15,18 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Random rand = new Random();
+        static Random rand = new Random();
 
-            int n, m, k,count=0;
-            int[] A,B,C;
+        static void Task1()
+        {
+            int n, m, k, count = 0;
+            int[] A, B, C;
             n = Convert.ToInt32(Console.ReadLine());
             m = Convert.ToInt32(Console.ReadLine());
             k = Convert.ToInt32(Console.ReadLine());
             A = new int[n];
             B = new int[m];
-            for(int i=0;i<n;i++)
+            for (int i = 0; i < n; i++)
             {
                 A[i] = rand.Next(-10, 10);
                 Console.Write($"{A[i]} ");
@@ -45,18 +45,61 @@ namespace ConsoleApp1
                 C[i] = A[i];
                 Console.Write($"{C[i]} ");
             }
-                int j = n;
-            for(int i=0;i<m;i++)
+            int j = n;
+            for (int i = 0; i < m; i++)
             {
                 if (B[i] % k == 0)
                 {
-                    C[j] = B[i];                    
+                    C[j] = B[i];
                     Console.Write($"{C[j]} ");
                     j++;
                 }
-                
+
             }
             Console.WriteLine();
+
+        }
+       static void Task2()
+        {
+            int n, min, pos=0;
+            int[] A;
+            n = Convert.ToInt32(Console.ReadLine());
+            A = new int[n];
+            min = A[0] = rand.Next(-10, 10);
+            Console.Write($"{A[0]} ");
+            for (int i = 1; i < n; i++)
+            {
+                A[i] = rand.Next(-10, 10);
+                Console.Write($"{A[i]} ");
+                if (A[i] < min)
+                {
+                    min = A[i];
+                    pos = i;
+                }     
+            }
+            Console.WriteLine();
+            pos++;
+            if(pos<n)
+            {
+                int[] B = new int[n - pos];
+                for (int i = pos; i < n; i++) B[i - pos] = A[i];
+                for(int i=0;i<(n-pos-1);i++)
+                    for(int j=i+1;j<(n-pos);j++)
+                    {
+                        if(B[i]>B[j])
+                        {
+                            int buff = B[i];
+                            B[i] = B[j];
+                            B[j] = buff;
+                        }
+                    }
+                for(int i=0;i<n-pos;i++) Console.Write($"{B[i]} ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Task2();
+
             Console.ReadKey();
         }
     }
